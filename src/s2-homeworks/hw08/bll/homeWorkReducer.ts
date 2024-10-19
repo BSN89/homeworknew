db02051d497f0388fe34926b51f0225405be8773
BSN89 <barakovsn55@gmail.com>
2024-10-19 11:28:10 +0300

    8 hw

--- src/s2-homeworks/hw08/bll/homeWorkReducer.ts
+++ src/s2-homeworks/hw08/bll/homeWorkReducer.ts
@@ -4,15 +4,21 @@ type ActionType =
     | { type: 'sort'; payload: 'up' | 'down' }
     | { type: 'check'; payload: number }
 
-export const homeWorkReducer = (state: any, action: any): any => { // need to fix any
+export const homeWorkReducer = (state: UserType[], action: ActionType): UserType[] => { // need to fix any
     switch (action.type) {
-        case 'sort': { // by name
-
-            return state // need to fix
+        case 'sort': {
+            if (action.payload === 'up'){
+                const copyState = state.map(el => el)
+                return copyState.sort((a,b) => b.age - a.age)
+            }if (action.payload === 'down'){
+                const copyState = state.map(el => el)
+                return copyState.sort((a,b) => a.age - b.age)
+            }
+                return state
         }
         case 'check': {
 
-            return state // need to fix
+            return state.filter(el => el.age >= action.payload)
         }
         default:
             return state
